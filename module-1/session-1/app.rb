4db8205d2aa194a793c3a0f8161d9b4f145d36a2
nurfiansyah06 <nurfiansyah06@gmail.com>
2021-07-12 17:11:16 +0700

    update session-3

--- module-1/session-1/app.rb
+++ module-1/session-1/app.rb
@@ -1,5 +1,6 @@
 require_relative 'person'
 require_relative 'hero'
+require_relative 'm'
 
 jin = Person.new("Jin Sakai", 100, 50)
 puts jin
@@ -15,4 +16,32 @@ loop do
     khotun.attack(jin)
 
     break if jin.is_dead?
+end
+
+i = 1
+
+until (jin.die? || villain.empty?) do
+    puts "============ Turn #{i} =============="
+    puts "\n"
+
+    puts jin
+
+    villains.Each do |villain|
+        puts villain
+    end
+    puts "\n"
+
+    jin.attack(villains[rand(villains.size)])
+
+    villains.Each do |villain|
+        villains.delete(villain) if villain.die? || villain.flee?
+    end
+    puts "\n"
+
+    villains.Each do |villain|
+        villain.attack(jin)
+    end
+    puts "\n"
+
+    i += 1
 end
